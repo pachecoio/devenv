ec59269b41e3aa56744d4fcc425a7ee91b437998
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2024-06-03 14:51:30 -0400

    changed wezterm theme

--- dotfiles/wezterm/.wezterm.lua
+++ dotfiles/wezterm/.wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.color_scheme = "AdventureTime"
+config.color_scheme = "tokyonight_night"
 
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.font_size = 16.0
